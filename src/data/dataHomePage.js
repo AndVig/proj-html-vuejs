7a41d761e4b6066b812b7e0217c3b2a49f0d1489
AlanDAmario <vega93@live.it>
2024-06-11 17:40:59 +0200

    [upd] add data elements

--- src/data/dataHomePage.js
+++ src/data/dataHomePage.js
@@ -8,4 +8,24 @@ export const dataHomePage = reactive({
       text: "Fractional or exclusive road cargo transportation to all regions, with small, medium and large vehicles.",
     },
   ],
+  transport: [
+    {
+      sub_title: "WHO WE ARE",
+      title: "Excellence in Transport",
+      pharagraph1:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum velit tortor, interdum sed cursus eu, sagittis ut nunc. Sed vitae tellus et arcu aliquet faucibus fermentum non lacus.",
+      pharagraph2:
+        "Praesent fringilla quis massa et placerat. Mauris eu dui eget urna pellentesque gravida vitae quis nibh. Ut at augue tortor. Pellentesque quis suscipit magna.",
+      pharagraph3:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam tortor, ultrices accumsan mauris eget, pulvinar tincidunt erat. Sed nisi nisi, rutrum sit amet elit.",
+      //TO DO RICONTROLLARE LI IN HOMEPAGE
+    },
+  ],
+  basicPlan: [
+    {
+      title: "Basic plan",
+      pharagraph: "lorem ipsum dolor sit amet,consectur adipiscing elit",
+      price: " $ 490 / mo",
+    },
+  ],
 });

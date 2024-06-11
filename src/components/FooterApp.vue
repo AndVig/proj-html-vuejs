2cbfbcee9e66f3c2695a2afd2ddc0bc58f1bc763
AndVig <andrea.vigna2@gmail.com>
2024-06-11 14:59:32 +0200

    UPD start footer

--- src/components/FooterApp.vue
+++ src/components/FooterApp.vue
@@ -1,12 +1,44 @@
 <script>
+import { dataFooter } from "../dataFooter.js";
 export default {
   name: "FooterApp",
+  data(){
+    return{
+        dataFooter,
+    }
+  }
 };
 </script>
 
 <template>
-  <h1>footer</h1>
-  <h2>prova</h2>
+  
+    <footer class="row row-cols-1 row-cols-sm-2 row-cols-md-5 py-5 my-5 border-top">
+        
+            <div class="col mb-3"></div>
+            <div class="col mb-3"></div>
+            <div v-for="card in dataFooter.cards" class="card col mb-3">
+                <h5>{{card.title}}</h5>
+                <ul class="nav flex-column">
+                  <li v-for="element in card.elements" class="nav-item mb-2">{{ element }}</li>
+                </ul>
+            
+        </div>
+        
+        
+    </footer>
+  
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    footer{
+        background-color: #111117;
+    }
+    .card{
+        background-color: #343a40;
+        z-index: 1;
+        padding: 35px;
+        border-radius: 10px;
+        border: none;
+        transition: all 0.4s ease-out 0s;
+    }
+</style>
---
+++ src/dataFooter.js
@@ -0,0 +1,35 @@
+import { reactive } from "vue";
+export const dataFooter= reactive({
+    cards:[
+        {
+            title:'Company',
+            elements:[
+                'Institutional',
+                'Social & Events',
+                'Innovation',
+                'Environment',
+                'Tecnology',
+            ]
+        },
+        {
+            title:'Services',
+            elements:[
+                'Audit & Assurance',
+                'Financial Advisory',
+                'Analytics and M&A',
+                'Middle Marketing',
+                'Legal Consulting',
+            ]
+        },
+        {
+            title:'Consultants',
+            elements:[
+                'David Cooper',
+                'Oliver Jones',
+                'Emma Lopez',
+                'T. Johnson',
+                'Jacob Hill Jr',
+            ]
+        },
+    ]
+})

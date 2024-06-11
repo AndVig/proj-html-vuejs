7d92d3a7a5ecfc9fe2a98013e3a8be04843b4c46
LucaPierantoni <luca.pier99@gmail.com>
2024-06-11 18:23:13 +0200

    UPD aggiunti props headerApp + button BUY

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,15 +1,27 @@
 <script>
+import { toRaw } from "vue";
 import FooterApp from "../components/FooterApp.vue";
 import HeaderApp from "../components/HeaderApp.vue";
 import MainHomeApp from "../components/MainHomeApp.vue";
+import { dataHeader } from "../data/dataHeader";
 
 export default {
   name: "AppHome",
   components: { FooterApp, MainHomeApp, HeaderApp },
+  setup() {
+    const data = toRaw(dataHeader);
+    return {
+      data,
+    };
+  },
 };
 </script>
 <template>
-  <HeaderApp />
+  <HeaderApp
+    :telephoneNumber="this.data.contacts.telephoneNumber"
+    :mainMailAdress="this.data.contacts.mainMailAdress"
+    :openHours="this.data.contacts.openHours"
+  />
   <MainHomeApp />
   <FooterApp />
 </template>

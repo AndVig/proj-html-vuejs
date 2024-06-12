4f30ac0fb54c8de0a1ad17e7dd112eb88e72b4b5
LucaPierantoni <luca.pier99@gmail.com>
2024-06-12 17:20:17 +0200

    UPD aggiunta Result section

--- src/components/MainAboutComponents/ResultSection.vue
+++ src/components/MainAboutComponents/ResultSection.vue
@@ -1,7 +1,42 @@
 <script>
 export default {
   name: "ResultSection",
+  props: {
+    results: Object,
+  },
 };
 </script>
-<template></template>
-<style scoped lang="scss"></style>
+<template>
+  <div class="my-container">
+    <div>
+      <h6>WHAT ARE WE DOING</h6>
+      <h3>Results in Numbers</h3>
+      <p>
+        Our goal is to exceed expectations by delivering the best job possible.
+      </p>
+    </div>
+    <ul>
+      <li>
+        <span>128</span>
+        <span>Certifications</span>
+      </li>
+      <li>
+        <span>230</span>
+        <span>Employees</span>
+      </li>
+      <li>
+        <span>517</span>
+        <span>Customers</span>
+      </li>
+      <li>
+        <span>94</span>
+        <span>Countries Served</span>
+      </li>
+    </ul>
+  </div>
+</template>
+<style scoped lang="scss">
+section {
+  width: 100%;
+}
+</style>

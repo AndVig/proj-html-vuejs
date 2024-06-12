6ae48233eafe10dd6b03c46d329a41778fecf893
AndVig <andrea.vigna2@gmail.com>
2024-06-12 15:33:56 +0200

    UPD aggiunto bullet png e continuato contact page

--- src/components/MainContactApp.vue
+++ src/components/MainContactApp.vue
@@ -10,11 +10,16 @@ export default {
   <main>
     <div class="background">
       <div class="overlay">
-        <div class="container text-center front">
-        <h2>Contact Us</h2>
-        
+        <div class="container d-flex justify-content-center inner ">
+          <div class="text-align-center text-white">
+            <h2>Contact Us</h2>
+            <p id="home" class="text-center">Home <span id="contactUs" class="text-center">/ Contact Us</span></p>
+          </div>
         </div>
       </div>
+    </div>
+    <div class="divider">
+      <div  ></div>
     </div>
   </main>
 </template>
@@ -26,6 +31,41 @@ export default {
   background-size: cover;
   background-position: center;
   width: 100%;
+}
+.inner{
+  padding-top: 100px;
+  padding-bottom: 100px;
+}
+.divider{
+  position: absolute;
+  z-index: 1;
+  right: 0;
+  left: calc(50% - 200px) ;
+  top: 326px;
+  background-repeat: no-repeat;
+  width: 420px;
+  height: 200px;
+  text-align: center;
+  opacity: 0.35;
+  scale: 30%;
+  background-image: url(../assets/img/bullets_sm.png);
+}
+#home{
+  font-size: 1rem;
+  font-weight: 200;
+  line-height: 1.5;
+}
+#home:hover{
+  color:#00A6A6;
+}
+#contactUs{
+  color:#00A6A6;
+}
+h2{
+  color: white;
+  font-size: 4.5rem;
+  font-weight: 900;
+  line-height: 1.2;
 }
 .overlay {
   background-color: rgba(28, 28, 28, 0.724); /* Colore scuro con opacità */

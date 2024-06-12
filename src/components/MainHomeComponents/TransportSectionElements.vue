21e4aa1a4d700cf724b76312bd8f88dd7942b739
AlanDAmario <vega93@live.it>
2024-06-12 17:19:36 +0200

    [upd] add transport part

--- src/components/MainHomeComponents/TransportSectionElements.vue
+++ src/components/MainHomeComponents/TransportSectionElements.vue
@@ -35,12 +35,66 @@ export default {
         </li>
       </ul>
     </div>
-    <div class="col-4">
-      <div class="bg-secondary-subtle">
-        <font-awesome-icon
-          :icon="['fas', 'truck-ramp-box']"
-          class="font-size"
-        />
+    <div class="col-4 p-4 ms-3 mt-5">
+      <div class="bg-secondary-subtle rounded-4 mt-5">
+        <div class="content pt-4 px-4">
+          <div
+            class="mb-2 d-flex font-size justify-content-between water-color"
+          >
+            <font-awesome-icon :icon="['fas', 'truck-ramp-box']" />
+            <font-awesome-icon
+              :icon="['fas', 'arrow-right']"
+              class="small-font mb-3 pb-3"
+            />
+          </div>
+          <div>
+            <h4 class="mb-3">
+              <strong>{{ dataHomePage.transport.basicPlan.title }}</strong>
+            </h4>
+            <p class="light-grey">
+              {{ dataHomePage.transport.basicPlan.pharagraph }}
+            </p>
+            <div class="water-color">
+              <span
+                ><font-awesome-icon
+                  :icon="['fas', 'dollar-sign']"
+                  class="font-medium"
+              /></span>
+              <span class="font-price"> <strong>{{
+                dataHomePage.transport.basicPlan.price
+              }}</strong></span>
+              <span class="font-period">{{
+                dataHomePage.transport.basicPlan.period
+              }}</span>
+            </div>
+            <ul class="py-3 ps-1 no-style">
+              <li
+                v-for="offer in dataHomePage.transport.basicPlan.offers"
+                class="my-4"
+              >
+                <div
+                  v-if="offer.flag"
+                  class="check d-flex justify-content-between"
+                >
+                  <span class="light-grey">{{ offer.name }}</span>
+                  <span
+                    ><font-awesome-icon
+                      :icon="['fas', 'check']"
+                      class="water-color small-font"
+                  /></span>
+                </div>
+                <div v-else class="cross d-flex justify-content-between">
+                  <span class="light-grey">{{ offer.name }}</span>
+                  <span
+                    ><font-awesome-icon
+                      :icon="['fas', 'xmark']"
+                      class="water-color small-font"
+                  /></span>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -52,6 +106,9 @@ export default {
   font-size: 15px;
   font-weight: bold;
   margin-top: 100px;
+}
+.water-color {
+  color: #058283;
 }
 
 h2 {
@@ -72,5 +129,28 @@ h2 {
 
 .font-size {
   font-size: 40px;
+}
+.small-font {
+  font-size: 25px;
+}
+
+.no-style li {
+  list-style-type: none;
+}
+.bg-secondary-subtle:hover {
+  transform: translateY(-10px);
+  transition: 0.8s;
+}
+
+.font-medium {
+  font-size: 50px;
+}
+.font-period {
+  font-size: 35px;
+  vertical-align: super;
+}
+.font-price {
+  font-size: 40px;
+  vertical-align: super;
 }
 </style>
--- src/data/dataHomePage.js
+++ src/data/dataHomePage.js
@@ -16,20 +16,17 @@ export const dataHomePage = reactive({
 
     pharagraph: [
       {
-        text:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum velit tortor, interdum sed cursus eu, sagittis ut nunc. Sed vitae tellus et arcu aliquet faucibus fermentum non lacus.",
-          flag: false,
-        },
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum velit tortor, interdum sed cursus eu, sagittis ut nunc. Sed vitae tellus et arcu aliquet faucibus fermentum non lacus.",
+        flag: false,
+      },
       {
-        text:
-          "Praesent fringilla quis massa et placerat. Mauris eu dui eget urna pellentesque gravida vitae quis nibh. Ut at augue tortor. Pellentesque quis suscipit magna.",
-          flag: false,
-        },
+        text: "Praesent fringilla quis massa et placerat. Mauris eu dui eget urna pellentesque gravida vitae quis nibh. Ut at augue tortor. Pellentesque quis suscipit magna.",
+        flag: false,
+      },
       {
-        text:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam tortor, ultrices accumsan mauris eget, pulvinar tincidunt erat. Sed nisi nisi, rutrum sit amet elit.",
+        text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam tortor, ultrices accumsan mauris eget, pulvinar tincidunt erat. Sed nisi nisi, rutrum sit amet elit.",
         flag: true,
-        },
+      },
     ],
     list: [
       {
@@ -47,11 +44,12 @@ export const dataHomePage = reactive({
     ],
     basicPlan: {
       title: "Basic plan",
-      pharagraph: "lorem ipsum dolor sit amet,consectur adipiscing elit",
-      price: " $ 490 / mo",
+      pharagraph: "Lorem ipsum dolor sit amet,consectur adipiscing elit",
+      price: " 490",
+      period: " / mo",
       offers: [
         {
-          name: "fraction load",
+          name: "Fraction load",
           flag: true,
         },
         {

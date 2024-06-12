50bb6b1e3ad576b9cfcd137abd55a45617c59750
AndVig <andrea.vigna2@gmail.com>
2024-06-12 17:33:55 +0200

    UPD add icons

--- src/main.js
+++ src/main.js
@@ -17,8 +17,11 @@ import { faArrowRight as arrow } from "@fortawesome/free-solid-svg-icons";
 import { faCheck as check } from "@fortawesome/free-solid-svg-icons";
 import { faXmark as cross } from "@fortawesome/free-solid-svg-icons";
 import { faDollarSign as dollar } from "@fortawesome/free-solid-svg-icons";
+import { faPhone as phone } from "@fortawesome/free-solid-svg-icons";
+import { faEnvelope as mail } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot as map } from "@fortawesome/free-solid-svg-icons";
 /* add icons to the library */
-library.add([secretAgent, fb, cargo, arrow, dollar, check, cross]);
+library.add([secretAgent, fb, cargo, arrow, dollar, check, cross, phone, mail, map]);
 
 createApp(App)
   .use(router)

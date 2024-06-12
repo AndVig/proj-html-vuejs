46f2d20bb769af99e7d1b859f2e5b2610f260bf4
AlanDAmario <vega93@live.it>
2024-06-12 13:46:27 +0200

    [upd] add elements

--- src/components/MainHomeComponents/CargoSectionElements.vue
+++ src/components/MainHomeComponents/CargoSectionElements.vue
@@ -8,12 +8,12 @@ export default {
   <!-- INTERPOLAZIONE DELLE PROPS DAL COMPONENTS CARGOSECTION  -->
 
   <div class="col-4 ms-5">
-    <h6 class="text-uppercase clr-sub-text pt-4">{{ subTitle }}</h6>
+    <h6 class="text-uppercase clr-sub-text pt-4 ">{{ subTitle }}</h6>
     <h1 class="text-uppercase display-2 text-white fw-bold py-3">
       {{ title }}
     </h1>
     <p class="pb-3">{{ text }}</p>
-    <div class="d-flex gap-3 pb-5">
+    <div class="d-flex gap-3 pd-bottom pt-3">
       <button
         type="button"
         class="btn btn-touch fs-6 px-4 rounded-pill btn-lg text-white"
@@ -33,13 +33,14 @@ export default {
 <style lang="scss" scoped>
 .clr-sub-text {
   color: #00a6a6;
+  font-size: 15px;
+  font-weight: bold;
 }
 
 p {
   width: 120%;
   color: rgba($color: #fdfbfb, $alpha: 0.7);
 }
-
 
 .btn-touch {
   background-color: #00a6a6;
@@ -53,5 +54,9 @@ p {
 }
 .btn-read span:hover {
   color: white;
+}
+
+.pd-bottom{
+padding-bottom: 100px;
 }
 </style>
--- src/data/dataHomePage.js
+++ src/data/dataHomePage.js
@@ -1,6 +1,7 @@
 //DATA HOME
 import { reactive } from "vue";
 export const dataHomePage = reactive({
+  //STRUTTURA DATI PRIMA PARTE (CARGO)
   cargo: [
     {
       sub_title: "logistic services",
@@ -8,33 +9,68 @@ export const dataHomePage = reactive({
       text: "Fractional or exclusive road cargo transportation to all regions, with small, medium and large vehicles.",
     },
   ],
-
+  //STRUTTURA DATI SECONDA PARTE (TRANSPORT)
   transport: {
     sub_title: "WHO WE ARE",
     title: "Excellence in Transport",
-    //  TO DO CONTROLLARE COME FATTO PE ROFFERS PER I PARAGRAFI
-    pharagraph1:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum velit tortor, interdum sed cursus eu, sagittis ut nunc. Sed vitae tellus et arcu aliquet faucibus fermentum non lacus.",
-    pharagraph2:
-      "Praesent fringilla quis massa et placerat. Mauris eu dui eget urna pellentesque gravida vitae quis nibh. Ut at augue tortor. Pellentesque quis suscipit magna.",
-    pharagraph3:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam tortor, ultrices accumsan mauris eget, pulvinar tincidunt erat. Sed nisi nisi, rutrum sit amet elit.",
-    //TO DO RICONTROLLARE LI IN HOMEPAGE, FARE ANCHE QUI COME OFFERS
-  },
 
-  basicPlan: {
-    title: "Basic plan",
-    pharagraph: "lorem ipsum dolor sit amet,consectur adipiscing elit",
-    price: " $ 490 / mo",
-    offers: [
+    pharagraph: [
       {
-        name: "fraction load",
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum velit tortor, interdum sed cursus eu, sagittis ut nunc. Sed vitae tellus et arcu aliquet faucibus fermentum non lacus.",
+          flag: false,
+        },
+      {
+        text:
+          "Praesent fringilla quis massa et placerat. Mauris eu dui eget urna pellentesque gravida vitae quis nibh. Ut at augue tortor. Pellentesque quis suscipit magna.",
+          flag: false,
+        },
+      {
+        text:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quam tortor, ultrices accumsan mauris eget, pulvinar tincidunt erat. Sed nisi nisi, rutrum sit amet elit.",
         flag: true,
+        },
+    ],
+    list: [
+      {
+        text: "Lorem ipsum dolor sit amet",
       },
       {
-        name: "Refrigerated cargo",
-        flag: true,
+        text: "Consectetur adipiscing elit",
+      },
+      {
+        text: "Integer molestie lorem at massa",
+      },
+      {
+        text: "Facilisis in pretium nisl aliquet",
       },
     ],
+    basicPlan: {
+      title: "Basic plan",
+      pharagraph: "lorem ipsum dolor sit amet,consectur adipiscing elit",
+      price: " $ 490 / mo",
+      offers: [
+        {
+          name: "fraction load",
+          flag: true,
+        },
+        {
+          name: "Refrigerated cargo",
+          flag: true,
+        },
+        {
+          name: "Regional delivery",
+          flag: true,
+        },
+        {
+          name: "Hazardous cargo",
+          flag: false,
+        },
+        {
+          name: "Mobile Tracking",
+          flag: false,
+        },
+      ],
+    },
   },
 });

2d7b81ddc615a0e6be20d3afc6f3880936a1ece9
LucaPierantoni <luca.pier99@gmail.com>
2024-06-13 09:25:25 +0200

    UPD Aggiunto Header su tutte le pagine

--- src/pages/AppAbout.vue
+++ src/pages/AppAbout.vue
@@ -2,15 +2,25 @@
 import FooterApp from "../components/FooterApp.vue";
 import HeaderApp from "../components/HeaderApp.vue";
 import MainAboutApp from "../components/MainAboutApp.vue";
+import { dataHeader } from "../data/dataHeader";
 
 export default {
   name: "AppAbout",
   components: { HeaderApp, FooterApp, MainAboutApp },
+  setup() {
+    return {
+      data: dataHeader,
+    };
+  },
 };
 </script>
 
 <template>
-  <HeaderApp />
+  <HeaderApp
+    :telephoneNumber="data.contacts.telephoneNumber"
+    :mainMailAdress="data.contacts.mainMailAdress"
+    :openHours="data.contacts.openHours"
+  />
   <MainAboutApp />
   <FooterApp />
 </template>
--- src/pages/AppContact.vue
+++ src/pages/AppContact.vue
@@ -2,14 +2,24 @@
 import FooterApp from "../components/FooterApp.vue";
 import HeaderApp from "../components/HeaderApp.vue";
 import MainContactApp from "../components/MainContactApp.vue";
+import { dataHeader } from "../data/dataHeader";
 
 export default {
   name: "AppContact",
   components: { HeaderApp, FooterApp, MainContactApp },
+  setup() {
+    return {
+      data: dataHeader,
+    };
+  },
 };
 </script>
 <template>
-  <HeaderApp />
+  <HeaderApp
+    :telephoneNumber="data.contacts.telephoneNumber"
+    :mainMailAdress="data.contacts.mainMailAdress"
+    :openHours="data.contacts.openHours"
+  />
   <MainContactApp />
   <FooterApp />
 </template>
